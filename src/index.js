0d3cbce925bcc8caa94b758aa95689675c65062f
pseuyi <pseuyi@gmail.com>
2019-11-04 17:23:20 -0500

    fix router issues

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 
-import App from './components/App'
-import './css/index.css'
+import App from './components/App';
+import './css/index.css';
 
+const baseUrl = process.env.PUBLIC_URL;
 ReactDOM.render(
-  (
-    <Router history={browserHistory}>
-      <Route path="/" component={App}>
-      </Route>
-    </Router>
-  ),
-document.getElementById('app'))
+  <Router history={browserHistory}>
+    <Route path={baseUrl + '/'} component={App}></Route>
+  </Router>,
+  document.getElementById('app'),
+);
